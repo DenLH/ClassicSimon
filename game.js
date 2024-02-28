5f3413f887b5a8d6ba8fb240d7bdfe2919639da2
DenLH <47705954+DenLH@users.noreply.github.com>
2024-02-27 22:08:46 -0500

    Add files via upload

---
+++ game.js
@@ -0,0 +1,104 @@
+//Empty array to receive patterns
+var gamePattern = [];
+
+//Empty array to store users clicks
+var userClickedPattern = [];
+
+//Array of all the button colors
+var buttonColors = [
+    "red",
+    "blue",
+    "green",
+    "yellow"
+];
+
+var started = false; //Boolean trigger
+var level = 0; //Initial level
+
+$(document).keydown(function() {
+    if (!started) { //Checks value of trigger
+        $("#level-title").text("Level " + level); //Updates the title text to show the current level
+        nextSequence(); //Starts the game
+        started = true; //Flips the trigger so this function can not be called again.
+    } 
+});
+
+//Use jquery to check when any button is clicked.
+$(".btn").click(function() {
+    //Create a variable that stores the id of a clicked button.
+    var userChosenColor = $(this).attr("id");
+    //Add the id of the clicked button to the array.
+    userClickedPattern.push(userChosenColor);
+
+    //playSound now takes an input parameter and then runs through the function to play the sound of the User Selected color
+    playSound(userChosenColor); 
+    //Flashes the pressed tag on the chosen button
+    animatePress(userChosenColor); 
+
+    checkAnswer(userClickedPattern.length-1);
+
+    console.log(userClickedPattern);
+});
+
+
+function nextSequence() {
+
+    level++;
+    $("#level-title").text("Level " + level);
+
+    var randomNumber = Math.floor(Math.random() * 4); //Generates a random number 0-3. Math floor rounds down.
+    var randomChosenColor = buttonColors[randomNumber]; //Selects an entry in the array for the value gained from random number.
+    gamePattern.push(randomChosenColor); //Assigns the random color to the gamePattern array.
+
+    $("#" + randomChosenColor).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100); //Makes the selected button flash
+    playSound(randomChosenColor); //playSound now takes an input parameter and then runs through the function to play the sound of the chosen color
+    
+    userClickedPattern = []; //Resets the array at the start of a new round. Makes it to where you have to select all the previous colors to match the gamePattern array
+
+};
+
+function playSound(name) {
+    var audio = new Audio("sounds/" + name + ".mp3");
+    audio.play(); //Plays the sound associated with the chosen color
+
+};
+
+function animatePress(currentColor) {
+    $("#" + currentColor).addClass("pressed"); //Adds the class pressed to the selected button
+        setTimeout(function() {
+            $("#" + currentColor).removeClass("pressed");// Removes the class from the selected button after 100ms
+        }, 100);
+    }
+    
+function checkAnswer(currentLevel) {
+    if (userClickedPattern[currentLevel] === gamePattern[currentLevel]) { //Compares entries of both arrays to verify
+        console.log("Success");
+
+
+    if (gamePattern.length === userClickedPattern.length) { //If arrays are equal, starts next round.
+        setTimeout(function() {
+            nextSequence();
+             
+        },1000);
+    }
+
+    } else {
+        playSound("wrong");
+        $("body").addClass("game-over")
+            setTimeout(function() {
+                $("body").removeClass("game-over");
+            }, 200);
+        $("#level-title").text("Game Over, Press Any Key to Restart");
+        startOver();
+        console.log("Wrong!");
+    };
+
+   
+};
+
+function startOver() {
+    level = 0;
+    gamePattern = [];
+    started = false;
+}
+
